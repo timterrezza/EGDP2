aea87fbe3ce91002c160e7cf82424d3168afe240
Tim Terrezza <terret@rpi.edu>
2014-09-10 15:18:35 -0400

    added incremental enemy movement

---
+++ Game/Assets/Resources/Scripts/enemyManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyManager : MonoBehaviour {
+	public GameObject[] enemies;
+
+	// Use this for initialization
+	void Start () {
+		enemies = GameObject.FindGameObjectsWithTag("enemy");
+	}
+
+	public void stepEnemies () {
+		for (int i = 0; i < enemies.Length; i++) {
+			enemies[i].gameObject.transform.localPosition += new Vector3(0f,0f,1f);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Game/Assets/Resources/Scripts/playerTileInteraction.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerTileInteraction : MonoBehaviour {
+	GameObject enemyManager;
+	// Use this for initialization
+	void Start () {
+		enemyManager = GameObject.Find ("Enemy Manager");
+	}
+
+	void OnCollisionExit (Collision c) {
+		if (c.gameObject.tag == "tile") {
+			enemyManager.GetComponent<enemyManager>().stepEnemies();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position = GameObject.Find ("First Person Controller").gameObject.transform.localPosition;
+		gameObject.transform.rotation = GameObject.Find ("First Person Controller").gameObject.transform.localRotation;
+	}
+}
