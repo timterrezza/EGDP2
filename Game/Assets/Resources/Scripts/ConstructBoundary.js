8a6ad3d349a5082ce7de76890839e5012cc3b3b0
Karl Olsen <olsenk2@rpi.edu>
2014-09-10 17:49:25 -0400

    Mapmaker- UPDATED

--- Game/Assets/Resources/Scripts/ConstructBoundary.js
+++ Game/Assets/Resources/Scripts/ConstructBoundary.js
@@ -10,35 +10,30 @@ var white = true;
 
 function Awake () {
     var map = mapAsset.text.Split ("\n"[0]);
-    var v = new Vector3 ();
-    var t = new Vector3 ();
-    v.y = 5.5;
-    t.y = 0;
-    var j_off = map.length / 50.0;
+    var x = 0.0;
+    var y = 5.5;
+    var z = 0.0;
     for (var j = 0; j < map.length; j ++) {
     	// * 4 = 4 meters between the center of each wall
-        v.z = (map.length - j - j_off + 4) * 4;
-        t.z = v.z;
         var i_off = map[j].length / 20.0;
         for (var i = 0; i < map[j].length; i ++) {
-        	//* 4 = 4 meters between the center of each wall
-            v.x = (i - i_off - 0.1) * 4;
-            t.x = v.x;
             if (map[j][i] == "X") {
-                Instantiate (wallPrefab, v, Quaternion.identity);
+                Instantiate (wallPrefab, new Vector3(i*4, y, j*4), Quaternion.identity);
             }
+            
             if(white == true)	{
-            	Instantiate (whiteTile, t, Quaternion.identity);
+            	Instantiate (whiteTile, new Vector3(i*4, 0, j*4), Quaternion.identity);
             	white = false;
             	if(i == map[j].Length - 1)
             		white = true;
             }
             else	{
-            	Instantiate (blackTile, t, Quaternion.identity);
+            	Instantiate (blackTile, new Vector3(i*4, 0, j*4), Quaternion.identity);
             	white = true;
             	if(i == map[j].Length - 1)
             		white = false;
             }
+            
         }
     }
 }
