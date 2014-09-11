022c9212a15dc4fb021a77403dffac67aa55dc24
mocchii <d.dan342@gmail.com>
2014-09-10 20:41:52 -0400

    Updated

--- Game/Assets/DogMove.cs
+++ Game/Assets/DogMove.cs
@@ -7,6 +7,10 @@ public class DogMove : MonoBehaviour {
 	Vector3 target;
 	private float cd = 1f;
 	private float next;
+	public int movement = 3;
+	private int curr = 1;
+	public Vector3[] point = new Vector3[4];
+	private Vector3 test;
 	
 	// Use this for initialization
 	void Start () {
@@ -18,12 +22,34 @@ public class DogMove : MonoBehaviour {
 	void Update () {
 		
 		
-		if(next + cd < Time.time){
-			MoveR();
-			next = Time.time;
-		}
+
 		float step = speed * Time.deltaTime;
 		transform.position = Vector3.MoveTowards(transform.position, target, step);
+		test = new Vector3(transform.position.x - point[curr].x, transform.position.y - point[curr].y,transform.position.z - point[curr].z);
+		if(test.x == 0 && test.z == 0){
+			curr++;
+			if(curr >= point.GetLength(0)){
+				curr = 0;
+			}
+		}
+	}
+	public void Calc(){
+		test = new Vector3(transform.position.x - point[curr].x, transform.position.y - point[curr].y,transform.position.z - point[curr].z);
+		if(test.x >= 1){
+			MoveU();
+		}else
+		if(test.x <= -1){
+			MoveD();
+		}else
+		if(test.z >= 1){
+			MoveR();
+		}else
+		if(test.z <= -1){
+			MoveL();
+		}
+
+
+
 	}
 	public void MoveR(){
 		target = new Vector3(transform.position.x,transform.position.y,transform.position.z - 4);
