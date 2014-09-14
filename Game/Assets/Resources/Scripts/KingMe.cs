ccf853341f9d516f46d1aa6162db6616d5007823
mocchii <d.dan342@gmail.com>
2014-09-14 16:54:49 -0400

    New prefab

---
+++ Game/Assets/Resources/Scripts/KingMe.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KingMe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter(Collision collision) {
+		Debug.Log("hit me");
+		if(collision.gameObject.name == "player"){
+			Application.LoadLevel("Resources/Scenes/Karl_Scene");
+
+		}
+	}
+}
