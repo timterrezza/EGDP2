9fe89f4455258f101ac51f3d88396cb6372f09e0
mocchii <d.dan342@gmail.com>
2014-09-16 22:15:55 -0400

    Added death scenes and sounds

---
+++ Game/Assets/die.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class die : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider c) {
+		if (c.gameObject.tag == "Pawn") {
+
+		}
+	}
+}
---
+++ Game/Assets/pewpew.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pewpew : MonoBehaviour {
+	public float speed = 20;
+	public float fadeSpeed = 1.5f;   
+	public AudioClip gun;
+	private bool ded = false;
+	void Start () {
+		ded = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(ded){
+			GameObject.FindGameObjectWithTag("fader").GetComponent<fader>().EndScene();
+		}
+	}
+	void OnTriggerEnter(Collider c) {
+		if (c.gameObject.tag == "player") {
+			//Debug.Log ("hi");
+			c.audio.clip = gun;
+			c.audio.Play();
+			ded = true;
+
+		}
+	}
+
+}
